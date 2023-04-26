358e378970533ede887ea8596c6a4aba82dd5ae0
Andrey Plekhanov <ierehon91@gmial.com>
2023-04-26 09:09:28 +0300

    Добавлен класс Settings для сохранения настроек игры в данном классе

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,18 @@
 import sys
-
 import pygame
+from settings import Settings
 
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-    bg_color = (230, 230, 230)
 
     # Запуск основного цикла игры
     while True:
         # Отслеживание событий клавиатуры и мыши
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         for event in pygame.event.get():
             if event.type is pygame.QUIT:
                 sys.exit()
