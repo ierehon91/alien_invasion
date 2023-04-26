91b81f5c6a39884182e55b2efea46692674ad78a
Andrey Plekhanov <ierehon91@gmial.com>
2023-04-26 14:04:50 +0300

    Рефакторинг. Вынос обработки нажатия и отпускания клавиш в отдельные функции

--- game_functions.py
+++ game_functions.py
@@ -5,20 +5,28 @@ from settings import Settings
 from ship import Ship
 
 
+def check_keydown_events(ship: Ship, event):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(ship: Ship, event):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship: Ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(ship, event)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(ship, event)
 
 
 
